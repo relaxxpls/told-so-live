0ff277b67805ef4562a2bbb60332dcd8468b0bb3
Laxman Desai <lolopaw24@gmail.com>
2022-11-22 20:16:17 +0530

    👌 IMPROVE: ADd deploy and hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+	solidity: '0.8.17',
+
+	networks: {
+		mumbai: {
+			chainId: 80001,
+			url: 'https://matic-mumbai.chainstacklabs.com',
+			accounts: [
+				'c6a25afe4fcd31836ef93b226bf626a91bcb2ae549d432d4966406256914e60b',
+			],
+		},
+	},
+}
